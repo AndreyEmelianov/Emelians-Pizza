1102ad1d36456d4e7ccd9bce0fe28ef3a52cb2c7
Andrey Emelianov <621077@mail.ru>
2023-09-11 19:15:02 +0300

    добавил разметку из готового html файла с версткой и пока что разбил на основные компоненты header, pizzaBlock, sort, categories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Categories from './components/Categories/Categories';
+import Header from './components/Header/Header';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import Sort from './components/Sort/Sort';
+
+import './scss/app.scss';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
