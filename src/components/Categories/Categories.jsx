3f5813daa29ae403c1abaf5f373deae85bc9dc44
Andrey Emelianov <621077@mail.ru>
2023-09-11 20:44:49 +0300

    перенес названия категорий в массив и отрендерил их

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onChangeCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            onClick={() => onChangeCategory(index)}
+            className={activeIndex === index ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
