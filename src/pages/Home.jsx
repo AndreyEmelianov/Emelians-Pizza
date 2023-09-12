27ab998e71dae852b9fe56a80d3befb373354420
Andrey Emelianov <621077@mail.ru>
2023-09-12 21:00:03 +0300

    подключил react-paginate реализова компонент Pagination  и сделал пагинацию страниц, пришлось захардкодить кол-во страниц, так как бекенд не умеет возвращать кол-во страниц

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from 'react';
+
 import Categories from '../components/Categories/Categories';
 import Sort from '../components/Sort/Sort';
 import Skeleton from '../components/PizzaBlock/Skeleton/Skeleton';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
+import Pagination from '../components/Pagination/Pagination';
 
 const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
   const [sortType, setSortType] = useState({ name: 'популярности', sortProperty: 'rating' });
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setIsLoading(true);
 
     const search = searchValue ? `&search=${searchValue}` : '';
     fetch(
-      `https://6500496918c34dee0cd4a89a.mockapi.io/items?${
+      `https://6500496918c34dee0cd4a89a.mockapi.io/items?page=${currentPage}&limit=4&${
         categoryId > 0 ? `category=${categoryId}` : ''
       }&sortBy=${sortType.sortProperty}&order=desc${search}`,
     )
@@ -25,7 +28,7 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, searchValue]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   const pizzas = items.map((item) => <PizzaBlock {...item} key={item.id} />);
   const skeletons = [...new Array(10)].map((_, index) => <Skeleton key={index} />);
@@ -38,6 +41,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
